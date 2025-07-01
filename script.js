450498b35ebda3987d6f7d240fff94634abf27bb
KrithikaRajesh <krithirajesh@gmail.com>
2025-07-01 11:24:43 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.getElementById("menu-toggle");
-  const menu = document.getElementById("menu");
-  toggle.addEventListener("click", () => {
-    menu.style.display = menu.style.display === "block" ? "none" : "block";
-  });
-});
